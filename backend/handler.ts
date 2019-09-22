54e64ce0192e6f8739104654d397636cf438408c
Nicolaus Christian <nicolauscg@gmail.com>
2019-09-22 19:17:07 +0700

    add serverless offline, introspect

--- backend/handler.ts
+++ backend/handler.ts
@@ -1,12 +1,29 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { ApolloServer } from 'apollo-server-lambda';
+import lambdaPlayground from 'graphql-playground-middleware-lambda';
+import { schema } from './src/graphql/schema';
 import 'source-map-support/register';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
-}
+require('dotenv').config();
+
+const server = new ApolloServer({
+  schema,
+  formatError: error => {
+    return error;
+  },
+  formatResponse: response => {
+    return response;
+  },
+  introspection: true
+});
+
+exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: '*'
+  }
+});
+
+exports.playgroundHandler = lambdaPlayground({
+  endpoint: process.env.IS_OFFLINE ?
+    'http://localhost:3000/graphql-introspect':
+    `/${process.env.STAGE}/graphql-introspect`
+});
