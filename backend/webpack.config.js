d7b9ac8b7b954b3eee79827929b959c3086a1c91
Nicolaus Christian <nicolauscg@gmail.com>
2019-09-22 19:17:07 +0700

    add serverless offline, introspect

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
-
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 module.exports = {
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
-  devtool: 'source-map',
+  devtool: ' cheap-module-eval-source-map',
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    extensions: ['.mjs', '.js', '.jsx', '.json', '.ts', '.tsx'],
   },
   output: {
     libraryTarget: 'commonjs',
@@ -14,10 +14,31 @@ module.exports = {
     filename: '[name].js',
   },
   target: 'node',
-  module: {
+module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              experimentalWatchApi: true,
+            },
+          },
+        ]
+      },
     ],
   },
+optimization: {
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: false,
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      useTypescriptIncrementalApi: true
+    }),
+  ]
 };
