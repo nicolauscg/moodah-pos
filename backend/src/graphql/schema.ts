60595f54af2492e448c1e1f1f069b215f90d8bd4
Michael Sudirman <astradon.michael@gmail.com>
2019-10-03 14:40:41 +0700

    finished creating stock.location.id schema

--- backend/src/graphql/schema.ts
+++ backend/src/graphql/schema.ts
@@ -34,7 +34,7 @@ const rootType = new GraphQLObjectType({
 
           const operation = dataSet.createSearchRead({
             modelName: "pos.config",
-            fields: ["name", "active"],
+            fields: ["name", "active","stock_location_id"],
             domain: []
           })
 
@@ -50,6 +50,13 @@ const rootType = new GraphQLObjectType({
                   }))
                 },
                 (result) => {
+                  result.records.forEach(index_i => {
+                    let printed_var = index_i.stock_location_id;
+                    printed_var.forEach(index_j => {
+                      console.log(index_j);
+                    });
+                  });
+                  console.log(result)
                   resolve(result.records)
                 }
               )
--- backend/src/graphql/schemas/posConfig.ts
+++ backend/src/graphql/schemas/posConfig.ts
@@ -1,5 +1,7 @@
-import { GraphQLObjectType, GraphQLBoolean, GraphQLString } from "graphql";
+// import { GraphQLObjectType, GraphQLBoolean, GraphQLString, GraphQLList} from "graphql";
+import { GraphQLObjectType, GraphQLBoolean, GraphQLString, GraphQLInt} from "graphql";
 import { globalIdField } from "graphql-relay";
+import { resolve } from "dns";
 
 const PosConfigType = new GraphQLObjectType({
   name: "PosConfig",
@@ -10,8 +12,29 @@ const PosConfigType = new GraphQLObjectType({
     },
     active: {
       type: GraphQLBoolean
+    },
+    stock_location_id: {
+      type: StockLocationType
     }
   })
 });
+
+const StockLocationType = new GraphQLObjectType({
+  name: "StockLocation",
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      resolve:(parent) =>{
+        return parent[1]
+      }
+    },
+    usage: {
+      type: GraphQLInt,
+      resolve:(parent) =>{
+        return parent[0]
+      }
+    }
+  })
+})
 
 export { PosConfigType };
