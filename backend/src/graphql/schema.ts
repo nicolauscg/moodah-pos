59d566ae51c07763cc4d32d5639826edc9594348
Michael Sudirman <astradon.michael@gmail.com>
2019-10-10 22:15:18 +0700

    [GREEN] implementing filter function to pass the filter test

--- backend/src/graphql/schema.ts
+++ backend/src/graphql/schema.ts
@@ -40,6 +40,23 @@ const POS_CONFIG_FIELDS = [
   "picking_type_id"
 ];
 
+function createDomainFilter(args) {
+  if (args.where === undefined) {
+    return [];
+  }
+  const result = [];
+  const data = args.where;
+
+  if (data.name !== undefined) {
+    result.push(["name", "ilike", data.name]);
+  }
+  if (data.stockLocationName !== undefined) {
+    result.push(["stock_location_id", "ilike", data.stockLocationName]);
+  }
+
+  return result;
+}
+
 const rootType = new GraphQLObjectType({
   name: "Query",
   fields: () => ({
@@ -49,7 +66,22 @@ const rootType = new GraphQLObjectType({
     },
     posConfigs: {
       type: GraphQLList(PosConfigType),
-      resolve: (_0, _1, context) =>
+      args: {
+        where: {
+          type: new GraphQLInputObjectType({
+            name: "PosConfigsInput",
+            fields: () => ({
+              name: {
+                type: GraphQLString
+              },
+              stockLocationName: {
+                type: GraphQLString
+              }
+            })
+          })
+        }
+      },
+      resolve: (_0, args, context) =>
         new Promise((res, rej) => {
           configureService({
             operation: getDataSet({
@@ -57,7 +89,7 @@ const rootType = new GraphQLObjectType({
             }).createSearchRead({
               modelName: "pos.config",
               fields: POS_CONFIG_FIELDS,
-              domain: []
+              domain: createDomainFilter(args)
             }),
             onError: error => {
               rej(
