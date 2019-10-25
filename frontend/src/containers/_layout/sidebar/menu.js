165a954e6c1f9c27e40e9b238a7266ecd892355f
Andhar1 <andhardezan@gmail.com>
2019-10-25 09:08:59 +0700

    Create Product Category menu in sidebar

--- frontend/src/containers/_layout/sidebar/menu.js
+++ frontend/src/containers/_layout/sidebar/menu.js
@@ -4,4 +4,9 @@ export default {
     route: '/partners/list',
     icon: 'partner',
   },
+  'moodah_general_menu.menu_productcategory': {
+      name: 'Product Category',
+      route: '/product_category/list',
+      icon: 'product',
+    },
 }
