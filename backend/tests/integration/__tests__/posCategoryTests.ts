abbde3b718509a0e37da992c71191f94c0b379f5
Michael Sudirman <astradon.michael@gmail.com>
2019-10-28 12:54:05 +0700

    [RED] add delete and update tests

--- backend/tests/integration/__tests__/posCategoryTests.ts
+++ backend/tests/integration/__tests__/posCategoryTests.ts
@@ -53,6 +53,29 @@ const CREATE_POS_CATEGORY = gql`
     }
   }
 `;
+const getUpdatePostCategoryQuery = (fieldsToUpate: string) => gql`
+  mutation {
+    updatePosCategory(input: ${fieldsToUpate}) {
+      success
+      posCategory {
+        id
+        name
+      }
+    }
+  }
+`;
+const getDeletePosCategoryQuery = (id: number) => gql`
+mutation {
+  deletePosCategory(input: {
+    id: ${id}
+  }) {
+    success
+    posCategory {
+      id
+      name
+    }
+  }
+}`;
 
 describe("Query", () => {
   it("query pos categories without session token give error", async () => {
@@ -94,5 +117,56 @@ describe("Mutation", () => {
     const result = (await query({ query: CREATE_POS_CATEGORY })).data
       .createPosCategory;
     expect(result.id).not.toBeNull();
+  });
+
+  it("create pos category, update, then delete", async () => {
+    const UPDATED_POS_CATEGORY_NAME = "updatedFromTest";
+    const server = await createTestServerWithSessionToken({
+      signInGql: SIGN_IN
+    });
+    const { query } = createTestClient(server);
+    const createResult = (await query({ query: CREATE_POS_CATEGORY })).data
+      .createPosCategory;
+    expect(createResult.id).not.toBeNull();
+
+    const { mutate } = createTestClient(server);
+    const createdPosCategoryId = createResult.id;
+    const updateResult: any = await mutate({
+      mutation: getUpdatePostCategoryQuery(`{
+        id: ${createdPosCategoryId},
+        name: "${UPDATED_POS_CATEGORY_NAME}"
+      }`)
+    });
+
+    expect(updateResult.data.updatePosCategory.posCategory.name).toEqual(
+      UPDATED_POS_CATEGORY_NAME
+    );
+    const deleteResult: any = await mutate({
+      mutation: getDeletePosCategoryQuery(createdPosCategoryId)
+    });
+    expect(deleteResult.data.deletePosCategory.success).toEqual(true);
+  });
+
+  it("update pos category without session token give error", async () => {
+    const server = createTestServer();
+    const { mutate } = createTestClient(server);
+    const result: any = await mutate({
+      mutation: getUpdatePostCategoryQuery(`{
+          id: -1,
+          name: "new name"
+        }`)
+    });
+
+    expect(result.errors).toEqual(expect.anything());
+  });
+
+  it("delete pos category without session token give error", async () => {
+    const server = createTestServer();
+    const { mutate } = createTestClient(server);
+    const result: any = await mutate({
+      mutation: getDeletePosCategoryQuery(-1)
+    });
+
+    expect(result.errors).toEqual(expect.anything());
   });
 });
