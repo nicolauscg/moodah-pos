9f9dae5be69643259922dd658f6f0e2d728151c6
Nicolaus Christian Gozali <nicolaus.christian@ui.ac.id>
2019-11-14 16:55:59 +0700

    [GREEN] add simple mocked category index  test

---
+++ frontend/src/containers/product_category/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProductCategoryIndex from './index';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+jest.mock('./components/ProductCategoryTable');
+
+it('renders index without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Router>
+      <ProductCategoryIndex />
+    </Router>
+  , div);
+  ReactDOM.unmountComponentAtNode(div);
+});
