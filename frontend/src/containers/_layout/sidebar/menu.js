c627ea18583a1871a137d83dbbfd0fdae6a55b7b
Andhar1 <andhardezan@gmail.com>
2019-11-15 22:19:27 +0700

    [CHORES] Create Product menu in the sidebar

--- frontend/src/containers/_layout/sidebar/menu.js
+++ frontend/src/containers/_layout/sidebar/menu.js
@@ -1,22 +1,27 @@
 export default {
+  'moodah_general_menu.menu_dashboard2': {
+    name: 'Dashboard',
+    route: '/dashboard/list',
+    icon: 'dasbor',
+  },
+  'moodah_general_menu.menu_productcategory': {
+    name: 'Product Category',
+    route: '/product_category/list',
+    icon: 'product',
+  },
+  'moodah_general_menu.menu_product': {
+    name: 'Product',
+    route: '/product/list',
+    icon: 'product',
+  },
+  'moodah_general_menu.menu_configuration': {
+    name: 'Configuration',
+    route: '/configuration/list',
+    icon: 'setting',
+  },
   'moodah_general_menu.menu_contacts': {
     name: 'Contacts',
     route: '/partners/list',
     icon: 'partner',
   },
-  'moodah_general_menu.menu_configuration': {
-    name: 'Configuration',
-    route: '/configuration/list',
-     icon: 'setting',
-  },
-  'moodah_general_menu.menu_productcategory': {
-      name: 'Product Category',
-      route: '/product_category/list',
-      icon: 'product',
-    },
-    'moodah_general_menu.menu_dashboard2': {
-      name: 'Dashboard',
-      route: '/dashboard/list',
-      icon: 'dasbor',
-    },
 }
