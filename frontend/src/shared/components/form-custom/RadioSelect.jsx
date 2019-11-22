55a26f42ab4414276eaa47aa41cdcd16af503993
Nicolaus Christian Gozali <nicolaus.christian@ui.ac.id>
2019-11-22 13:08:30 +0700

    [CHORES] add radio group component

--- frontend/src/shared/components/form-custom/RadioSelect.jsx
+++ frontend/src/shared/components/form-custom/RadioSelect.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import clsx from 'clsx';
-import { withStyles, makeStyles, createStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import { FieldArray } from "formik";
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import { compose,withState } from 'recompose'
 
+// source https://material-ui.com/components/radio-buttons/#customized-radios
 const StyledRadio = withStyles({
   root: {
     '&:hover': {
@@ -45,22 +49,54 @@ const StyledRadio = withStyles({
       backgroundColor: '#106ba3',
     },
   },
-})(({classes})=>(
+})(({classes, ...props})=>(
   <Radio
     className={classes}
-    disableRipple
     color="default"
     checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
     icon={<span className={classes.icon} />}
+    {...props}
   />))
 
-export default function CustomizedRadios() {
+const RadioSelect = ({
+  name,
+  label,
+  options,
+  selected,
+  setSelected
+}) => {
   return (
-    <FormControl component="fieldset">
-      <RadioGroup defaultValue="female" aria-label="gender" name="customized-radios">
-        <FormControlLabel value="female" control={<StyledRadio />} label="Tax Included" />
-        <FormControlLabel value="male" control={<StyledRadio />} label="Tax Excluded" />
-      </RadioGroup>
-    </FormControl>
+    <FieldArray
+      name={name}
+      render={arrayHelpers => (
+        <FormControl component="fieldset">
+          <FormLabel component="legend">{label}</FormLabel>
+          <RadioGroup defaultValue="female" aria-label="gender" name="customized-radios">
+            {options.map(option => 
+              <FormControlLabel
+                label={option.label}
+                value={option.value}
+                control={
+                  <StyledRadio
+                    checked={selected === option.value}
+                    onChange={event => {
+                      arrayHelpers.pop();
+                      if (event.target.checked) {
+                        setSelected(option.value)
+                        arrayHelpers.push(option.value);
+                      }
+                    }}
+                  />
+                }
+              />
+            )}
+          </RadioGroup>
+        </FormControl>
+      )}
+    />
   );
 }
+
+export default compose(
+  withState('selected', 'setSelected', null),
+)(RadioSelect);
