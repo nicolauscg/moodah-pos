61c967905d48400974e84cca5d4af5b16f2b84cf
Nicolaus Christian <nicolaus.christian@ui.ac.id>
2019-11-25 12:26:20 +0700

    [CHORES] add product index table

--- frontend/src/utils/transformers/product.tsx
+++ frontend/src/utils/transformers/product.tsx
@@ -1,14 +1,23 @@
-import { toSuggestions } from "./general";
-import { PosProductsSelect } from "../../generated-pos-models";
+import { PosProducts } from "../../generated-pos-models";
+
+// ====================================================
+// Constants
+// ====================================================
+export const ProductColumns = [
+  { name: "internalReference", title: "Internal Reference" },
+  { name: "name", title: "Name" },
+  { name: "salesPrice", title: "Sales Price" },
+  { name: "cost", title: "Cost" },
+  { name: "internalCategory", title: "Internal Category" },
+  { name: "productType", title: "Product Type" }
+];
 
 // ====================================================
 // Transformers
 // ====================================================
-export const prepareProducts = (posProducts: PosProductsSelect.PosProducts) => {
-  return {
-    posProducts: {
-      ...posProducts,
-      records: toSuggestions(posProducts.records)
-    }
-  };
+export const prepareProductRows = (rows: Array<PosProducts.Records>) => {
+  return rows.map(row => ({
+    ...row,
+    internalCategory: row.category !== null ? row.category.name : row.category
+  }));
 };
