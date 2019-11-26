6fa2fb0b5979bd95c1ea035388db8161abec779f
Andhar1 <andhardezan@gmail.com>
2019-11-26 07:38:48 +0700

    Creating some static components for the pos session page

--- frontend/src/containers/pos_session/index.jsx
+++ frontend/src/containers/pos_session/index.jsx
@@ -1,63 +1,86 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { debounce } from 'lodash'
 import { compose, withState, withPropsOnChange, withHandlers } from 'recompose'
+import { withRouter } from 'react-router-dom'
 
 import { Col, Container, Row, Button } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import SearchIcon from 'mdi-react/SearchIcon'
 import { withFormik, Form, Field } from 'formik'
 
+import { PosCategories } from '../../generated-pos-models'
+import ChartDatePicker from '../../shared/components/ChartDatePicker'
 import offsetToCursor from '../../utils/offsetToCursor'
-
 import Breadcrumb from '../../shared/components/Breadcrumb'
 
-const SearchInput = ({ field, form, handleSetValue, ...props }) => {
-  const { onChange, ...restField } = field
-  return (
-    <input
-      {...restField}
-      {...props}
-      onChange={e => {
-        form.setFieldValue(field.name, e.target.value)
+import DropzoneGQL from '../../shared/components/Dropzone.jsx'
+import Modal from '../../shared/components/form-custom/Modal'
 
-        if (handleSetValue) {
-          handleSetValue(e.target.value)
-        }
-      }}
-      type="text"
+const RemoveImageModal = ({ toggle, isOpen, confirm }) => {
+  return (
+    <Modal
+      type='dialog'
+      title='Remove Profile Picture Image'
+      body='Are you sure you want to remove this image?'
+      action={
+        <Fragment>
+          <Button color='primary' size='sm' onClick={confirm}>
+            Yes
+          </Button>
+          <Button color='danger' size='sm' onClick={toggle}>
+            No
+          </Button>
+        </Fragment>
+      }
+      toggle={toggle}
+      isOpen={isOpen}
+      centered
     />
   )
 }
 
-const SearchForm = ({ handleSetValue }) => {
-  return (
-    <Form className="search">
-      <Field
-        name="keyword"
-        className="search-field"
-        placeholder="Search for items to order ..."
-        handleSetValue={handleSetValue}
-        component={SearchInput}
-      />
-      <button className="search-btn" type="submit">
-        <SearchIcon />
-      </button>
-    </Form>
-  )
+const ImageField = ({
+  imageField,
+  setImageField,
+  isInUpdateImage,
+  toggleRemoveImage,
+  setIsInUpdateImage
+}) => {
+  if (!isInUpdateImage && imageField) {
+    return (
+      <Fragment>
+          <img
+            src={`data:image/png;base64,${imageField}`}
+            className='.image-field'
+            alt='category-pic'
+          />
+          <Button color='warning' size='sm' onClick={() => setIsInUpdateImage(true)}>
+            Change
+          </Button>
+          <Button color='danger' size='sm' onClick={toggleRemoveImage}>
+            Remove
+          </Button>
+      </Fragment>
+    )
+  } else {
+    return (
+      <Fragment>
+        <DropzoneGQL
+          loading={false}
+          uploader={(encodedImage) => {
+            setImageField(encodedImage);
+            setIsInUpdateImage(false);
+          }}
+        />
+        {isInUpdateImage && (
+          <Button color='danger' size='sm' onClick={() => setIsInUpdateImage(false)}>
+            Cancel
+          </Button>
+        )}
+      </Fragment>
+    )
+  }
 }
-
-const FormikSearch = compose(
-  withFormik({
-    mapPropsToValues: ({ filters }) => ({
-      keyword: filters.name_contains,
-    }),
-    handleSubmit: () => {},
-    enableReinitialize: true,
-  }),
-  withPropsOnChange(['handleSetValue'], ({ handleSetValue }) => ({
-    handleSetValue: debounce(handleSetValue, 1000),
-  }))
-)(SearchForm)
 
 const Pos_SessionIndex = ({
   filters,
@@ -65,45 +88,162 @@ const Pos_SessionIndex = ({
   offset,
   setOffset,
   handleSetValue,
+  productcategory,
+  toggleRemoveImage,
+  removeImageModalIsOpen,
+  removeImageConfirm,
+  refetchParents,
+  ...props
 }) => {
   return (
     <Container className="configuration__list">
-      <Row className="header">
+       <Row className="header">
         <Col md={4} className="header__item">
-          <Breadcrumb crumbs={[{ text: 'Pos Session' }]} />
         </Col>
-        <Col
-          md={8}
-          className="header__item d-flex align-items-center justify-content-end"
-        >
-           <FormikSearch
-            filters={filters}
-            setFilters={setFilters}
-            setOffset={setOffset}
-            handleSetValue={handleSetValue}
-           />
+         <Col
+           md={8}
+           className="header_item d-flex align-items-center justify-content-end"
+         >
+           <ChartDatePicker />
+         </Col>
+       </Row>
 
-          <Link
-          to={`/configuration/create`}
-          className="btn btn-primary btn-sm"
-          >
-          Buat Baru
-          </Link>
+       <hr />
 
-          <Button size="sm" color="help" tag="a" href="mailto:support@rubyh.co">
-          Bantuan
-          </Button>
+        <Row className="header">
+          <Col md={10}>
+             <div className="example1">
+                <Col md={8} className="header__item">
+                  <Breadcrumb crumbs={[{ text: 'Pos Session' }]} />
+                </Col>
+                <Col >
+                  <ImageField toggleRemoveImage={toggleRemoveImage} {...props} />
+                </Col>
+                <Breadcrumb crumbs={[{ text: 'Samuel Adi Prasetyo' }]}  />
+                <Breadcrumb crumbs={[{ text: 'Front Cashier' }]} />
+                <Button color='primary' size='sm' type='submit'>
+                  Close Session
+                </Button>
+                <Breadcrumb crumbs={[{ text: '2019 All Rights Reserved' }]} />
+             </div>
+          </Col>
 
-        </Col>
-      </Row>
+          <Col md={2}>
+            <div className="example2">
+             <Col>
+               <Button className="btn btn-primary">
+                 Orders#4509
+               </Button>
+             </Col>
+            </div>
+          </Col>
+        </Row>
+
+      <RemoveImageModal
+              toggle={toggleRemoveImage}
+              isOpen={removeImageModalIsOpen}
+              confirm={removeImageConfirm}
+      />
     </Container>
   )
 }
 
+const Pos_SessionIndex2 = compose(
+    withRouter,
+    PosCategories.HOC({
+        name: 'parents',
+        options: ({
+          parentFilters
+        }) => ({
+          context: {
+            clientName: 'pos'
+          },
+          variables: {
+            filters: parentFilters,
+            offset: 0,
+            limit: 10
+          }
+        })
+      }),
+    withState('isInUpdateImage', 'setIsInUpdateImage', false),
+      withState(
+        'imageField',
+        'setImageField',
+        ({ productcategory }) => productcategory !== undefined && !productcategory.loading ?
+          productcategory.posCategory.image :
+          null
+      ),
+      withState('removeImageModalIsOpen', 'setRemoveImageModalIsOpen', false),
+      withState('parentFilters', 'setParentFilters', {}),
+      withHandlers({
+        toggleUpdatingImageState: ({ isInUpdateImage, setIsInUpdateImage }) =>
+          () => setIsInUpdateImage(!isInUpdateImage),
+        toggleRemoveImage: ({ removeImageModalIsOpen, setRemoveImageModalIsOpen }) =>
+          () => setRemoveImageModalIsOpen(!removeImageModalIsOpen)
+      }),
+      withHandlers({
+        removeImageConfirm: ({ setImageField, toggleRemoveImage }) => () => {
+          setImageField(null);
+          toggleRemoveImage();
+        },
+        refetchParents: ({ setParentFilters }) => input => {
+          setParentFilters({
+            OR: [{ 'name': input }]
+          })
+        },
+      }),
+      withFormik({
+          mapPropsToValues: props => {
+            const { productcategory } = props;
+            const posCategory = productcategory !== undefined && !productcategory.loading ?
+              productcategory.posCategory : null
 
-const defaultFilters = {
-    name_contains: '',
-    type: 'configuration',
-  }
+            if (posCategory) {
+              return {
+                ...posCategory,
+                parent: posCategory.parent ?
+                  ({
+                    ...posCategory.parent,
+                    label: posCategory.parent.displayName,
+                    value: posCategory.parent.id
+                  }) :
+                  null
+              };
+            }
 
-export default (Pos_SessionIndex)
+            return {
+              name: '',
+              parent: null,
+              sequence: '',
+            }
+          },
+          handleSubmit: (values, { props }) => {
+            if (values.name) {
+              props.handleSubmit({
+                context: {
+                  clientName: 'pos'
+                },
+                variables: {
+                  input: {
+                    id: values.id,
+                    name: values.name,
+                    image: props.imageField,
+                    parentId: values.parent ? values.parent.value : null,
+                    sequence: parseInt(values.sequence)
+                  }
+                }
+              })
+            } else {
+              props.triggerNotif({
+                message: 'Product Category Name is required!',
+                type: 'warning',
+              })
+            }
+          },
+          validateOnChange: false,
+          validateOnBlur: false,
+          enableReinitialize: true,
+  })
+)(Pos_SessionIndex)
+
+export default (Pos_SessionIndex2)
