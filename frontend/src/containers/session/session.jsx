f30f92e479612159b48e6ad4a22035aafe6fb01f
Nicolaus Christian <nicolaus.christian@ui.ac.id>
2019-12-10 21:55:32 +0700

    implement layout of session page

--- frontend/src/containers/session/session.jsx
+++ frontend/src/containers/session/session.jsx
@@ -9,31 +9,71 @@ import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
-import { compose } from "recompose";
+import { compose, withHandlers } from "recompose";
 
-import { SessionCategories, SessionProducts } from "../../generated-pos-models";
+import {
+  SessionCategories,
+  SessionProducts,
+  CloseSession
+} from "../../generated-pos-models";
 import Loader from "../../shared/components/Loader";
 import ProductCard from "../../shared/components/ProductCard";
 import Carousel from "../../shared/components/Carousel";
+import Card from "@material-ui/core/Card";
 
 const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  },
   palette: {
     primary: {
       main: "#289FD7",
-      secondary: "#EFFAFF"
+      constrastText: "#FFFFFF"
     },
-    text: {
-      primary: "#646777",
-      secondary: "#FFFFFF"
+    secondary: {
+      main: "#FFFFFF",
+      constrastText: "#646777"
     }
   }
 });
 
 const styles = theme => ({
+  primaryColor: {
+    color: theme.palette.secondary.constrastText
+  },
+  secondaryColor: {
+    color: `${theme.palette.primary.constrastText} !important`
+  },
+  primaryBg: {
+    backgroundColor: theme.palette.primary.main
+  },
+  secondaryBg: {
+    backgroundColor: "#EFFAFF"
+  },
+  primaryContainedButton: {
+    backgroundColor: "#289FD7",
+    color: "#FFFFFF"
+  },
+  secondaryContainedButton: {
+    backgroundColor: "#FFFFFF",
+    color: "#646777"
+  },
+  parentWidthHeight: {
+    width: "100%",
+    height: "100%"
+  },
+  parentWidth: {
+    width: "100%"
+  },
+  profileImage: {
+    borderRadius: "25%"
+  },
   productCol: {
     maxHeight: "100%",
+    flexGrow: 1,
     display: "flex",
-    flexDirection: "column"
+    flexDirection: "column",
+    height: "1px"
   },
   gridContainer: {
     overflowY: "scroll",
@@ -46,17 +86,167 @@ const styles = theme => ({
     }
   },
   mainRow: {
-    height: "calc(100vh - 80px)"
+    height: "100vh"
   },
   paper: {
     backgroundColor: theme.palette.primary.secondary
-  },
-  containedButton: {
-    color: theme.palette.text.secondary
   }
 });
 
-const Session = ({ classes, categories, products }) => {
+const ProfileColumn = ({ classes, onCloseSession }) => {
+  return (
+    <Card
+      className={`${classes.parentWidthHeight} ${
+        classes.primaryBg
+      } text-center p-3`}
+    >
+      <div
+        className={`${
+          classes.parentWidthHeight
+        } d-flex flex-column justify-content-between`}
+      >
+        <div>
+          <img
+            className={`${classes.profileImage} p-4`}
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR7ULXhTjsUrfpFmzCzUo8T_dMjCwwshvugIVID97Y_5KYouGDJ"
+          />
+          <Typography
+            classes={{ root: classes.secondaryColor }}
+            variant="h5"
+            component="h2"
+          >
+            Samuel
+          </Typography>
+          <Typography
+            classes={{ root: classes.secondaryColor }}
+            className="font-weight-light"
+            variant="subtitle1"
+            component="h3"
+          >
+            front cashier
+          </Typography>
+        </div>
+        <div className="d-flex flex-column">
+          <Button
+            variant="contained"
+            size="small"
+            className={`${classes.secondaryContainedButton} mb-2 py-2`}
+            onClick={onCloseSession}
+          >
+            Close session
+          </Button>
+          <Typography
+            classes={{ root: classes.secondaryColor }}
+            className="font-weight-light"
+            variant="subtitle2"
+            component="p"
+          >
+            2019 all rights
+          </Typography>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+const ProductColumn = ({
+  classes,
+  className,
+  categoryRecords,
+  productRecords,
+  onCloseSession
+}) => {
+  return (
+    <div className={className}>
+      <Typography variant="h6">Product Category</Typography>
+      <Carousel>
+        {categoryRecords.map(category => (
+          <ProductCard name={category.name} image={category.image} />
+        ))}
+      </Carousel>
+      <hr className={classes.parentWidth} />
+      <Typography variant="h6" className="mb-2">
+        Product List
+      </Typography>
+      <Grid container xs={12} className={classes.gridContainer} spacing={8}>
+        {productRecords.map(product => (
+          <Grid
+            key={product.id}
+            container
+            className={classes.gridItem}
+            justify="center"
+          >
+            <ProductCard
+              name={product.name}
+              image={product.image}
+              price={product.salesPrice}
+              className="mb-3"
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+const OrderColumn = ({ classes, className }) => {
+  return (
+    <div className={className}>
+      <Paper classes={{ root: classes.secondaryBg }} elevation={1}>
+        <Col xs={12} className="px-5 py-2">
+          <Row className="justify-content-between mb-2">
+            <Typography variant="h6" component="h3">
+              Items
+            </Typography>
+            <Typography variant="h6" component="h3">
+              Price
+            </Typography>
+          </Row>
+          <Row className="justify-content-between mb-2">
+            <Typography variant="body1" component="p">
+              Subtotal
+            </Typography>
+            <Typography variant="body1" component="p">
+              Rp 14.400
+            </Typography>
+          </Row>
+          <Row className="justify-content-between mb-2">
+            <Typography variant="body1" component="p">
+              Discount
+            </Typography>
+            <Typography variant="body1" component="p">
+              Rp 0
+            </Typography>
+          </Row>
+          <Row className="justify-content-between">
+            <Typography
+              variant="body1"
+              component="p"
+              className="font-weight-bold"
+            >
+              Total
+            </Typography>
+            <Typography
+              variant="body1"
+              component="p"
+              className="font-weight-bold"
+            >
+              Rp 134.000
+            </Typography>
+          </Row>
+        </Col>
+      </Paper>
+      <Button
+        variant="contained"
+        className={`${classes.primaryContainedButton} mt-3`}
+      >
+        Payment
+      </Button>
+    </div>
+  );
+};
+
+const Session = ({ classes, categories, products, onCloseSession }) => {
   const { loading: loadingCategories, posCategories } = categories;
   const { loading: loadingProducts, posProducts } = products;
 
@@ -68,92 +258,32 @@ const Session = ({ classes, categories, products }) => {
   const productRecords = posProducts.records;
 
   return (
-    <Container className={classes.container}>
+    <Container>
       <Row className={classes.mainRow}>
-        <Col xs={2} />
-        <Col xs={5} xl={4} className={classes.productCol}>
-          <Typography variant="h6">Product Category</Typography>
-          <Carousel>
-            {categoryRecords.map(category => (
-              <ProductCard name={category.name} image={category.image} />
-            ))}
-          </Carousel>
-          <Typography variant="h6" className="mb-2">
-            Product List
-          </Typography>
-          <Grid container xs={12} className={classes.gridContainer} spacing={8}>
-            {productRecords.map(product => (
-              <Grid
-                key={product.id}
-                container
-                className={classes.gridItem}
-                justify="center"
-              >
-                <ProductCard
-                  name={product.name}
-                  image={product.image}
-                  price={product.salesPrice}
-                  className="mb-3"
-                />
-              </Grid>
-            ))}
-          </Grid>
+        <Col xs={2} className="pl-0">
+          <ProfileColumn classes={classes} onCloseSession={onCloseSession} />
         </Col>
-        <Col xs={5} xl={6} className="d-flex flex-column">
-          <Paper classes={{ root: classes.paper }} elevation={1}>
-            <Col xs={12} className="px-5 py-2">
-              <Row className="justify-content-between mb-2">
-                <Typography variant="h6" component="h3" color="textPrimary">
-                  Items
-                </Typography>
-                <Typography variant="h6" component="h3" color="textPrimary">
-                  Price
-                </Typography>
-              </Row>
-              <Row className="justify-content-between mb-2">
-                <Typography variant="body1" component="p" color="textPrimary">
-                  Subtotal
-                </Typography>
-                <Typography variant="body1" component="p" color="textPrimary">
-                  Rp 14.400
-                </Typography>
-              </Row>
-              <Row className="justify-content-between mb-2">
-                <Typography variant="body1" component="p" color="textPrimary">
-                  Discount
-                </Typography>
-                <Typography variant="body1" component="p" color="textPrimary">
-                  Rp 0
-                </Typography>
-              </Row>
-              <Row className="justify-content-between">
-                <Typography
-                  variant="body1"
-                  component="p"
-                  color="textPrimary"
-                  className="font-weight-bold"
-                >
-                  Total
-                </Typography>
-                <Typography
-                  variant="body1"
-                  component="p"
-                  color="textPrimary"
-                  className="font-weight-bold"
-                >
-                  Rp 134.000
-                </Typography>
-              </Row>
+        <Col xs={10} className="d-flex flex-column">
+          <Row className="d-flex flex-column align-items-stretch pt-4">
+            <div className="text-right mr-2">
+              <h3>Thursday 24 July 2019, 09.00 AM</h3>
+              <h4>Shift Started at 08.00AM ( 1 :00 Hours )</h4>
+            </div>
+          </Row>
+          <hr className={classes.parentWidth} />
+          <Row className="flex-grow">
+            <Col xs={6} xl={5} className="d-flex flex-column">
+              <ProductColumn
+                classes={classes}
+                className={classes.productCol}
+                categoryRecords={categoryRecords}
+                productRecords={productRecords}
+              />
             </Col>
-          </Paper>
-          <Button
-            variant="contained"
-            color="primary"
-            classes={{ contained: classes.containedButton }}
-            className="mt-3"
-          >
-            Payment
-          </Button>
+            <Col xs={6} xl={7}>
+              <OrderColumn classes={classes} className="d-flex flex-column" />
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
@@ -167,6 +297,28 @@ const SessionPage = compose(
     </MuiThemeProvider>
   ),
   withStyles(styles),
+  WrappedComp => props => (
+    <CloseSession.Component onError={props.onError}>
+      {(closeSession, { loading }) => (
+        <WrappedComp
+          closeSession={closeSession}
+          loadingClose={loading}
+          {...props}
+        />
+      )}
+    </CloseSession.Component>
+  ),
+  withHandlers({
+    onCloseSession: ({ match, closeSession, history }) => () =>
+      closeSession({
+        context: {
+          clientName: "pos"
+        },
+        variables: {
+          id: match.params.id
+        }
+      }).then(() => history.push("/dashboard/list"))
+  }),
   SessionCategories.HOC({
     name: "categories",
     options: ({ filters, offset, limit }) => ({
