b4fd46312530dfa58a24ec31a2bfa6b9aba25148
Nicolaus Christian <nicolaus.christian@ui.ac.id>
2019-12-12 21:15:00 +0700

    Merge branch 'andhar/createnewform_posproduct_ui' into 'Sprint-4-US-1-2-3-continue'
    Andhar/createnewform posproduct ui
    
    See merge request it-project-kki/2019/rubyh-moodah-pos!53

---
+++ frontend/src/shared/components/ProductCard.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+import NoImage from "../../shared/img/pos/noImage.png";
+
+const styles = {
+  card: {
+    width: 110,
+    height: "fit-content"
+  },
+  media: {
+    height: 110,
+    objectFit: "cover"
+  },
+  name: {
+    lineHeight: 1.2
+  }
+};
+
+const MediaCard = ({ classes, className, name, image, price, onPress }) => {
+  return (
+    <Card className={`${classes.card} ${className}`} onClick={onPress}>
+      <CardActionArea>
+        <img
+          className={classes.media}
+          src={image ? `data:image/png;base64,${image}` : NoImage}
+        />
+        <CardContent className="p-1">
+          <Typography
+            className={`${classes.name} font-weight-bold`}
+            gutterBottom
+            variant="caption"
+            component="h2"
+          >
+            {name}
+          </Typography>
+          {price && (
+            <Typography variant="caption" component="p">
+              {`Rp. ${price}`}
+            </Typography>
+          )}
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default withStyles(styles)(MediaCard);
